b139e325bfa6ee632b29bf24ffed2d4a23400690
y-du <42994541+y-du@users.noreply.github.com>
2023-02-16 14:51:15 +0100

    remove unnecessary nil checks

--- validation/inputs.go
+++ validation/inputs.go
@@ -23,54 +23,48 @@ import (
 )
 
 func validateInputsResources(inputs map[string]model.Input, mResources map[string]model.Set[string]) error {
-	if inputs != nil {
-		for ref := range inputs {
-			if ref == "" {
-				return errors.New("invalid input reference")
+	for ref := range inputs {
+		if ref == "" {
+			return errors.New("invalid input reference")
+		}
+		if mResources != nil {
+			if _, ok := mResources[ref]; !ok {
+				return fmt.Errorf("resource '%s' not defined", ref)
 			}
-			if mResources != nil {
-				if _, ok := mResources[ref]; !ok {
-					return fmt.Errorf("resource '%s' not defined", ref)
-				}
-			} else {
-				return errors.New("no resources defined")
-			}
+		} else {
+			return errors.New("no resources defined")
 		}
 	}
 	return nil
 }
 
 func validateInputsSecrets(inputs map[string]model.Input, mSecrets map[string]model.Secret) error {
-	if inputs != nil {
-		for ref := range inputs {
-			if ref == "" {
-				return errors.New("invalid input reference")
+	for ref := range inputs {
+		if ref == "" {
+			return errors.New("invalid input reference")
+		}
+		if mSecrets != nil {
+			if _, ok := mSecrets[ref]; !ok {
+				return fmt.Errorf("secret '%s' not defined", ref)
 			}
-			if mSecrets != nil {
-				if _, ok := mSecrets[ref]; !ok {
-					return fmt.Errorf("secret '%s' not defined", ref)
-				}
-			} else {
-				return errors.New("no secrets defined")
-			}
+		} else {
+			return errors.New("no secrets defined")
 		}
 	}
 	return nil
 }
 
 func validateInputsConfigs(inputs map[string]model.Input, mConfigs model.Configs) error {
-	if inputs != nil {
-		for ref := range inputs {
-			if ref == "" {
-				return errors.New("invalid input reference")
+	for ref := range inputs {
+		if ref == "" {
+			return errors.New("invalid input reference")
+		}
+		if mConfigs != nil {
+			if _, ok := mConfigs[ref]; !ok {
+				return fmt.Errorf("config '%s' not defined", ref)
 			}
-			if mConfigs != nil {
-				if _, ok := mConfigs[ref]; !ok {
-					return fmt.Errorf("config '%s' not defined", ref)
-				}
-			} else {
-				return errors.New("no configs defined")
-			}
+		} else {
+			return errors.New("no configs defined")
 		}
 	}
 	return nil
@@ -91,15 +85,13 @@ func validateInputsAndGroups(inputs map[string]model.Input, groups map[string]mo
 }
 
 func validateInputGroups(groups map[string]model.InputGroup) error {
-	if groups != nil {
-		for ref, group := range groups {
-			if ref == "" {
-				return errors.New("invalid input group reference")
-			}
-			if group.Group != nil {
-				if _, ok := groups[*group.Group]; !ok {
-					return fmt.Errorf("input group '%s' not defined", *group.Group)
-				}
+	for ref, group := range groups {
+		if ref == "" {
+			return errors.New("invalid input group reference")
+		}
+		if group.Group != nil {
+			if _, ok := groups[*group.Group]; !ok {
+				return fmt.Errorf("input group '%s' not defined", *group.Group)
 			}
 		}
 	}
