d08b799fdeaa041263bf65a9e51fae1cf62e908b
y-du <42994541+y-du@users.noreply.github.com>
2023-02-17 12:38:03 +0100

    remove unnecessary nil check

--- validation/services.go
+++ validation/services.go
@@ -75,19 +75,17 @@ func validateServiceConfigs(sConfigs map[string]string, mConfigs model.Configs) 
 }
 
 func validateServiceHttpEndpoints(sHttpEndpoints map[string]model.HttpEndpoint, extPaths map[string]struct{}) error {
-	if sHttpEndpoints != nil {
-		for extPath, ept := range sHttpEndpoints {
-			if !isValidPath(extPath) {
-				return fmt.Errorf("invalid external path '%s'", extPath)
-			}
-			if !isValidPath(ept.Path) {
-				return fmt.Errorf("invalid internal path '%s'", ept.Path)
-			}
-			if _, ok := extPaths[extPath]; ok {
-				return fmt.Errorf("duplicate path '%s'", extPath)
-			}
-			extPaths[extPath] = struct{}{}
+	for extPath, ept := range sHttpEndpoints {
+		if !isValidPath(extPath) {
+			return fmt.Errorf("invalid external path '%s'", extPath)
 		}
+		if !isValidPath(ept.Path) {
+			return fmt.Errorf("invalid internal path '%s'", ept.Path)
+		}
+		if _, ok := extPaths[extPath]; ok {
+			return fmt.Errorf("duplicate path '%s'", extPath)
+		}
+		extPaths[extPath] = struct{}{}
 	}
 	return nil
 }
